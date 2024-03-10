8491276726d5f8b2afcd02a2f1b8834e0094214f
wiiiiltoon <wilbarca_02@hotmail.com>
2024-03-10 13:23:18 +0100

    [FEATURE/FIX] Se han arreglado los metodos de la clase producto y se han aniadido nuevos metodos para Producto y Version

--- estructuras/Producto.go
+++ estructuras/Producto.go
@@ -8,6 +8,10 @@ type Producto struct {
 	version    Version
 }
 
+func NewProducto(nombre, definicion string, precio float32, valoracion int) *Producto {
+	return &Producto{nombre, definicion, precio, valoracion, *NewVersion()}
+}
+
 func (p *Producto) getNombre() string {
 	return p.nombre
 }
@@ -28,22 +32,22 @@ func (p *Producto) getTimestamp() string {
 	return p.version.timeStamp
 }
 
-func (p *Producto) getVersion() int {
+func (p *Producto) getVersion() float32 {
 	return p.version.getVersion()
 }
 
-func (p *Producto) setNombre() string {
-	return p.nombre
+func (p *Producto) setNombre(nombre string) {
+	p.nombre = nombre
 }
 
-func (p *Producto) setDefinicion() string {
-	return p.nombre
+func (p *Producto) setDefinicion(definicion string) {
+	p.definicion = definicion
 }
 
-func (p *Producto) setPrecio() float32 {
-	return p.precio
+func (p *Producto) setPrecio(precio float32) {
+	p.precio = precio
 }
 
-func (p *Producto) setValoracion() int {
-	return p.valoracion
+func (p *Producto) setValoracion(valoracion int) {
+	p.valoracion = valoracion
 }
--- estructuras/Version.go
+++ estructuras/Version.go
@@ -1,12 +1,29 @@
 package estructuras
 
+import (
+	"strconv"
+	"time"
+)
+
 type Version struct {
-	version   int
+	version   float32
 	timeStamp string
 }
 
-func (v *Version) getVersion() int {
+func NewVersion() *Version {
+	return &Version{1.0, getTimeStampActual()}
+}
+
+func getTimeStampActual() string {
+	return strconv.Itoa(time.Now().Nanosecond())
+}
+
+func (v *Version) getVersion() float32 {
 	return v.version
+}
+
+func (v *Version) setNewVersion() float32 {
+	return v.version + 1
 }
 
 func (v *Version) getTimeStamp() string {
